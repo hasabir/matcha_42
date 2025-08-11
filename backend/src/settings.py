43f33491b9e2188bc81202706c3d499b9681b0d4
hasabir <sabirhajar048@gmail.com>
2025-08-11 23:03:23 +0100

    create user app

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -39,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'app',
+    'users',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
